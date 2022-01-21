eb37f941c60232514fe86340b57d9ddfbfb7ac5d
hmcvay <hayleymcvay8@gmail.com>
2022-01-21 10:11:19 -0800

    WIP: writing function for third spec test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 // BUSINESS LOGIC
 function beepBoop(num) {
-  let outputArray = [];
-  if ((isNaN(num))) {
+  const outputArray = [];
+  const inputArray = num.split("");
+  if ((isNaN(inputArray))) {
     return false;
-  } else if (num === 0) {
-    outputArray.push(0);
-      }
-  console.log(outputArray);
+  } else if (inputArray >= 0) {
+    outputArray.push("0");
+  } else if (num > 0) {
+  for (i = 0; i < inputArray; i++) {
+    if 
+
+    }
+  }
+  }
+  console.log (outputArray);
 }
